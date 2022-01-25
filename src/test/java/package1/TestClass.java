fb7d1a2cb57abfdc8ab076c3a9d253e4cc85966d
pradhanmol <pradhanmol30@gmail.com>
2022-01-25 15:36:12 +0530

    jenkinsproject

---
+++ src/test/java/package1/TestClass.java
@@ -0,0 +1,22 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+
+    }
+
+
+    @Test
+    public void test2(){
+        System.out.println("test2");
+
+    }
+
+
+
+}
